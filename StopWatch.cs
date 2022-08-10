91b9df24e41571249c4e23339b47cf89b36883e2
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:19:50 +0530

    Initial Push from local

---
+++ StopWatch.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalPractiseProblems
+{
+    class StopWatch
+    {
+        public static void StartStop()
+        {
+            Console.WriteLine("Question 6 : Stopwatch Program \n");
+            Console.WriteLine("\n >>>> Press Enter to START <<<<");
+
+            Console.ReadLine();
+            var start = DateTime.Now;
+
+            Console.WriteLine("\n >>>> Press Enter to Stop <<<<");
+            Console.ReadLine();
+
+            var end = DateTime.Now;
+
+            var timeElapsed = end - start;
+
+            Console.WriteLine("Time Elapsed : {0} Minutes and {1} Seconds and {2} Milliseconds", timeElapsed.Minutes, timeElapsed.Seconds, timeElapsed.Milliseconds);
+        }
+    }
+}
