c15617da0d69d8fcaeef577a68942053cb5703a1
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:19:40 +0530

    Initial Push from local

---
+++ PrimeNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace LogicalPractiseProblems
+{
+    class PrimeNumber
+    {
+        public static void CheckPrime()
+        {
+            Console.WriteLine("Question 3 : Prime Number check \n");
+            Console.Write("Enter a Number : ");
+
+            try
+            {
+                var input = Convert.ToInt32(Console.ReadLine());
+                var result = string.Format(" >>>> {0} is a PRIME number !",input);
+
+                if (input < 1)
+                    throw new Exception();
+
+                for (var i = 2; i < input/2; i++)
+                {
+                    if (input % i == 0)
+                        result = string.Format(">>>> {0} is NOT a prime number", input); break;
+                }
+                Console.WriteLine("\n {0}",result);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n !!!! INVALID INPUT !!!! ");
+                CheckPrime();
+            }
+        }
+    }
+}
