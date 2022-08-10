546c821d8e2d1fe21715b26be4116193d451af77
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:19:34 +0530

    Initial Push from local

---
+++ PerfectNumber.cs
@@ -0,0 +1,39 @@
+﻿namespace LogicalPractiseProblems
+{
+    class PerfectNumber
+    {
+        public static void CheckPerfect()
+        {
+            Console.WriteLine("Question 2 : Check Whether the number is Perfect or Not \n");
+            Console.Write("Enter a number : ");
+
+            try
+            {
+                var input = Convert.ToInt32(Console.ReadLine());
+
+                if (input < 1)
+                    throw new Exception();
+
+                var divisors = new List<int>();
+                for (var i = 1; i < input/2; i++)
+                {
+                    if (input%i == 0)
+                        divisors.Add(i);
+                }
+                var sum = 0;
+                foreach (var div in divisors)
+                    sum += div;
+
+                var result = (sum == input/2) ? string.Format("{0} is a PERFECT number", input) : string.Format("{0} is NOT a PERFECT number", input);
+
+                Console.WriteLine("\n >>>> {0} as the sum of divisors is : {1}",result,sum*2);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n !!!! INVALID INPUT !!!! ");
+                CheckPerfect();
+            }
+        }
+
+    }
+}
