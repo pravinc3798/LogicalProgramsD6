6e63fc1d688eed88f4d52fbdd2e155c673556a22
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:19:45 +0530

    Initial Push from local

---
+++ Reversion.cs
@@ -0,0 +1,30 @@
+﻿namespace LogicalPractiseProblems
+{
+    class Reversion
+    {
+        public static void GetReversed()
+        {
+            Console.WriteLine("Question 4 : Reverse whatever is provided \n");
+
+            try
+            {
+                Console.Write("Input something : ");
+                var input = Console.ReadLine();
+
+                var reversedInput = "";
+
+                for (var i = 0; i < input.Length; i++)
+                {
+                    reversedInput += input[(input.Length - i - 1)];
+                }
+
+                Console.WriteLine("\n >>>>> Reversed Output : {0}", reversedInput);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n!!! INVALID INPUT !!!\n");
+                GetReversed();
+            }
+        }
+    }
+}
